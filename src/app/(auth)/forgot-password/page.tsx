4a6461c7c3d177f9ef97117df211c979baef7ac9
Annette Theodore <annettetheodore582@gmail.com>
2023-11-23 16:30:00 +0000

    created forgot-password page

---
+++ src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,27 @@
+import ForgotPasswordForm from '@/components/forms/forgot-password-form'
+import Image from 'next/image'
+import React from 'react'
+export default function ForgotPasswordPage() {
+  return (
+
+    <main style={{
+        backgroundImage: `url("/assets/images/backgroundImg.png")`,
+        backgroundRepeat: "no-repeat",
+      }} className='min-h-screen flex items-center justify-center'>
+        <div className=' absolute top-4 left-4'>
+        <Image
+          src="/assets/images/companyLogo.png"
+          width={100}
+          height={100}
+          alt="Logo"
+        />
+        </div>
+      
+      <div className='max-w-lg w-full'>
+      <ForgotPasswordForm />
+      </div>
+    </main>
+      
+    
+  )
+}
